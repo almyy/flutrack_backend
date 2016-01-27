1c88c2fd75fabf65af5ff52a9d075fdaec888e0e
Martin Almvik <martalm@stud.ntnu.no>
2016-01-27 15:16:27 +0100

    Added communication between front-end and back-end

--- tweets/models.py
+++ tweets/models.py
@@ -1,6 +1,5 @@
 class Tweet(object):
-    def __init__(self, id, lat, lng, username, text):
-        self.id = id
+    def __init__(self, lat, lng, username, text):
         self.lat = lat
         self.lng = lng
         self.username = username
--- tweets/serializers.py
+++ tweets/serializers.py
@@ -3,7 +3,12 @@ from tweets.models import Tweet
 
 
 class TweetSerializer(serializers.Serializer):
-    id = serializers.CharField(required=True, max_length=50)
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
     lat = serializers.CharField(required=False, max_length=50)
     lng = serializers.CharField(required=False, max_length=50)
     username = serializers.CharField(required=True, max_length=50)
@@ -11,11 +16,10 @@ class TweetSerializer(serializers.Serializer):
 
     def restore_object(self, attrs, instance=None):
         if instance:
-            instance.id = attrs.get('id', instance.id)
             instance.lat = attrs.get('lat', instance.lat)
             instance.lng = attrs.get('lng', instance.lng)
             instance.username = attrs.get('username', instance.username)
             instance.text = attrs.get('text', instance.text)
             return instance
 
-        return Tweet(attrs.get('id'), attrs.get('lat'), attrs.get('lng'), attrs.get('username'), attrs.get('text'))
+        return Tweet(attrs.get('lat'), attrs.get('lng'), attrs.get('username'), attrs.get('text'))
