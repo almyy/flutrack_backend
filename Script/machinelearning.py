a58ac217373cb47fc18f6730e7f105c34510c71a
Martin Almvik <martalm@stud.ntnu.no>
2016-02-04 11:47:50 +0100

    First try. Everything's negative

--- Script/machinelearning.py
+++ Script/machinelearning.py
@@ -3,78 +3,79 @@ import re
 import nltk
 
 
-def process_tweet(tweet_text):
-    tweet_text = tweet_text.lower()
-    tweet_text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet_text)
-    tweet_text = re.sub('@[^\s]+', 'AT_USER', tweet_text)
-    tweet_text = re.sub('[\s]+', ' ', tweet_text)
-    tweet_text = re.sub(r'#([^\s]+)', r'\1', tweet_text)
-    tweet_text = tweet_text.strip('\'"')
-    return tweet_text
+class MachineLearning:
+    stop_words = ['AT_USER', 'URL']
+    NBClassifier = None
+    feature_list = []
 
+    @staticmethod
+    def process_tweet(tweet_text):
+        tweet_text = tweet_text.lower()
+        tweet_text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet_text)
+        tweet_text = re.sub('@[^\s]+', 'AT_USER', tweet_text)
+        tweet_text = re.sub('[\s]+', ' ', tweet_text)
+        tweet_text = re.sub(r'#([^\s]+)', r'\1', tweet_text)
+        tweet_text = tweet_text.strip('\'"')
+        return tweet_text
 
-stop_words = ['AT_USER', 'URL']
+    @staticmethod
+    def replace_two_or_more(s):
+        pattern = re.compile(r"(.)\1+", re.DOTALL)
+        return pattern.sub(r"\1\1", s)
 
-
-def replace_two_or_more(s):
-    pattern = re.compile(r"(.)\1+", re.DOTALL)
-    return pattern.sub(r"\1\1", s)
-
-
-def get_stop_word_list(stop_word_list_filename):
-    fp = open(stop_word_list_filename, 'r')
-    line = fp.readline()
-    while line:
-        word = line.strip()
-        stop_words.append(word)
+    def get_stop_word_list(self, stop_word_list_filename):
+        fp = open(stop_word_list_filename, 'r')
         line = fp.readline()
-    fp.close()
-    return stop_words
+        while line:
+            word = line.strip()
+            self.stop_words.append(word)
+            line = fp.readline()
+        fp.close()
+
+    def get_feature_vector(self, tweet):
+        featureVector = []
+        words = tweet.split()
+        for w in words:
+            w = self.replace_two_or_more(w)
+            w = w.strip('\'"?,.')
+            val = re.search(r"^[a-zA-Z][a-zA-Z0-9]*[a-zA-Z]+[a-zA-Z0-9]*$", w)
+            if w in self.stopWords or val is None:
+                continue
+            else:
+                featureVector.append(w.lower())
+        return featureVector
+
+    def extract_features(self, tweet):
+        tweet_words = set(tweet)
+        features = {}
+        for word in self.feature_list:
+            features['contains(%s)' % word] = (word in tweet_words)
+        return features
+
+    def classify(self, tweet):
+        self.classifier.classify(self.extract_features(self.get_feature_vector(tweet)))
+
+    def __init__(self):
+        # Read the tweets one by one and process it
+        inp_tweets = csv.reader(open('sampleTweets.csv', 'r', encoding='utf-8'), delimiter=',', quotechar='|')
+        tweets = []
+        for row in inp_tweets:
+            if row[0] == '+':
+                sentiment = 'positive'
+                # print(row[1].encode('utf-8'))
+            else:
+                sentiment = 'negative'
+            tweet = row[1]
+            processed_tweet = self.process_tweet(tweet)
+            feature_vector = self.get_feature_vector(processed_tweet)
+            tweets.append((feature_vector, sentiment))
+            self.feature_list.extend(feature_vector)
+
+        self.feature_list = list(set(self.feature_list))
+        training_set = nltk.classify.util.apply_features(self.extract_features, tweets)
+        self.classifier = nltk.NaiveBayesClassifier.train(training_set)
 
 
-def get_feature_vector(tweet, stopWords):
-    featureVector = []
-    words = tweet.split()
-    for w in words:
-        w = replace_two_or_more(w)
-        w = w.strip('\'"?,.')
-        val = re.search(r"^[a-zA-Z][a-zA-Z0-9]*[a-zA-Z]+[a-zA-Z0-9]*$", w)
-        if w in stopWords or val is None:
-            continue
-        else:
-            featureVector.append(w.lower())
-    return featureVector
-
-
-def extract_features(tweet):
-    tweet_words = set(tweet)
-    features = {}
-    for word in featureList:
-        features['contains(%s)' % word] = (word in tweet_words)
-    return features
-
-
-# Read the tweets one by one and process it
-inpTweets = csv.reader(open('sampleTweets.csv', 'r', encoding='utf-8'), delimiter=',', quotechar='|')
-tweets = []
-featureList = []
-for row in inpTweets:
-    if row[0] == '+':
-        sentiment = 'positive'
-        print(row[1].encode('utf-8'))
-    else:
-        sentiment = 'negative'
-    tweet = row[1]
-    processedTweet = process_tweet(tweet)
-    featureVector = get_feature_vector(processedTweet, stop_words)
-    tweets.append((featureVector, sentiment))
-    featureList.extend(featureVector)
-
-featureList = list(set(featureList))
-training_set = nltk.classify.util.apply_features(extract_features, tweets)
-
-NBClassifier = nltk.NaiveBayesClassifier.train(training_set)
-
-test_tweet = 'Sick flu headache fever'
-processedTweet = process_tweet(test_tweet)
-print(NBClassifier.classify(extract_features(get_feature_vector(processedTweet, stop_words))))
+        # test_tweet = 'fever headache'
+        # processedTweet = process_tweet(test_tweet)
+        # print(NBClassifier.classify(extract_features(get_feature_vector(processedTweet, stop_words))))
--- Script/twittering.py
+++ Script/twittering.py
@@ -1,8 +1,86 @@
-import tweepy
-import re
 import codecs
+import tweepy
+import csv
+import re
+import nltk
 
-f = codecs.open('sampleTweets.csv', 'w', encoding='utf-8')
+
+class MachineLearning:
+    stop_words = ['AT_USER', 'URL']
+    classifier = None
+    feature_list = []
+
+    @staticmethod
+    def process_tweet(tweet_text):
+        tweet_text = tweet_text.lower()
+        tweet_text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet_text)
+        tweet_text = re.sub('@[^\s]+', 'AT_USER', tweet_text)
+        tweet_text = re.sub('[\s]+', ' ', tweet_text)
+        tweet_text = re.sub(r'#([^\s]+)', r'\1', tweet_text)
+        tweet_text = tweet_text.strip('\'"')
+        return tweet_text
+
+    @staticmethod
+    def replace_two_or_more(s):
+        pattern = re.compile(r"(.)\1+", re.DOTALL)
+        return pattern.sub(r"\1\1", s)
+
+    def get_stop_word_list(self, stop_word_list_filename):
+        fp = open(stop_word_list_filename, 'r')
+        line = fp.readline()
+        while line:
+            word = line.strip()
+            self.stop_words.append(word)
+            line = fp.readline()
+        fp.close()
+
+    def get_feature_vector(self, tweet):
+        featureVector = []
+        words = tweet.split()
+        for w in words:
+            w = self.replace_two_or_more(w)
+            w = w.strip('\'"?,.')
+            val = re.search(r"^[a-zA-Z][a-zA-Z0-9]*[a-zA-Z]+[a-zA-Z0-9]*$", w)
+            if w in self.stop_words or val is None:
+                continue
+            else:
+                featureVector.append(w.lower())
+        return featureVector
+
+    def extract_features(self, tweet):
+        tweet_words = set(tweet)
+        features = {}
+        for word in self.feature_list:
+            features['contains(%s)' % word] = (word in tweet_words)
+        return features
+
+    def classify(self, tweet):
+        return self.classifier.classify(self.extract_features(self.get_feature_vector(tweet)))
+
+    def __init__(self):
+        # Read the tweets one by one and process it
+        inp_tweets = csv.reader(open('sampleTweets.csv', 'r', encoding='utf-8'), delimiter=',', quotechar='|')
+        tweets = []
+        for row in inp_tweets:
+            if row[0] == '+':
+                sentiment = 'positive'
+                print(row[1].encode('utf-8'))
+            else:
+                sentiment = 'negative'
+            tweet = row[1]
+            processed_tweet = self.process_tweet(tweet)
+            feature_vector = self.get_feature_vector(processed_tweet)
+            tweets.append((feature_vector, sentiment))
+            self.feature_list.extend(feature_vector)
+
+        self.feature_list = list(set(self.feature_list))
+        training_set = nltk.classify.util.apply_features(self.extract_features, tweets)
+        self.classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+
+        # test_tweet = 'fever headache'
+        # processedTweet = process_tweet(test_tweet)
+        # print(NBClassifier.classify(extract_features(get_feature_vector(processedTweet, stop_words))))
 
 
 def filter_tweet(status):
@@ -12,8 +90,8 @@ def filter_tweet(status):
         return True
 
 
-def store_status_ml(status):
-    f.write(",|" + status.text + '|\n')
+# def store_status_ml(status):
+#     f.write(",|" + status.text + '|\n')
 
 
 class FluStreamListener(tweepy.StreamListener):
@@ -21,13 +99,17 @@ class FluStreamListener(tweepy.StreamListener):
     status_count_retweet = 0
     previous_status_text = ""
     status_history = []
+    learner = MachineLearning()
 
     def on_status(self, status):
         if filter_tweet(status):
             self.status_count_original += 1
-            print(str(self.status_count_original) + " " + str(status.text.encode("utf-8")))
             # self.previous_status_text = re.sub(r"https?:\/\/(t\.co\/[a-zA-z]+)", "", status.text)
-            store_status_ml(status)
+            # store_status_ml(status)
+
+            test_tweet = status.text
+            processed_tweet = self.learner.process_tweet(test_tweet)
+            print(str(processed_tweet.encode('utf-8')) + ", " + self.learner.classify(processed_tweet))
         else:
             self.status_count_retweet += 1
             # print("Retweet count: " + str(self.status_count_retweet) + str(status.text.encode("utf-8")))
@@ -35,15 +117,15 @@ class FluStreamListener(tweepy.StreamListener):
     def on_error(self, status_code):
         print(str(status_code))
 
-    def store_status(self, status):
-        self.status_history.append(
-                str(self.status_count_original) + " User: " + str(status.user.screen_name) +
-                " Text: " + str(status.text.encode("utf-8")))
-
-    def save_history(self):
-        with codecs.open("status_dump.csv", "w", encoding='utf-8') as text_file:
-            for status in self.status_history:
-                text_file.write("\n" + str(status))
+    # def store_status(self, status):
+    #     self.status_history.append(
+    #             str(self.status_count_original) + " User: " + str(status.user.screen_name) +
+    #             " Text: " + str(status.text.encode("utf-8")))
+    #
+    # def save_history(self):
+    #     with codecs.open("status_dump.csv", "w", encoding='utf-8') as text_file:
+    #         for status in self.status_history:
+    #             text_file.write("\n" + str(status))
 
 
 def stream_tweets():
@@ -57,10 +139,12 @@ def stream_tweets():
     data = ['fever sick', 'fever cough', 'flu sick', 'flu fever', 'runny nose', 'stuffed nose',
             'sick cough', 'fever cough', 'sore throat', 'headache fever', 'fatigued sick', 'fever tired',
             'vomiting flu', 'chills flu']
+
     try:
         stream.filter(track=data)
     except (KeyboardInterrupt, AttributeError) as e:
         print(e)
 
 
-stream_tweets()
+if __name__ == '__main__':
+    stream_tweets()
