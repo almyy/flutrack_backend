d32ac09c089e1a99a3c263d4a58021d662c30054
Martin Almvik <martalm@stud.ntnu.no>
2016-02-17 14:09:47 +0100

    olinrgolrjngol

--- flutrack_backend/populateDB.py
+++ flutrack_backend/populateDB.py
@@ -1,7 +1,9 @@
 from pymongo import MongoClient
 import requests
+from pymongo.errors import OperationFailure
 
 client = MongoClient('mongodb://heroku_k99m6wnb:slu38scru44f1c5s2v4h60ig72@ds011238.mongolab.com:11238/heroku_k99m6wnb')
+print('client connected')
 db = client.flutrack_db
 tweets = db.tweets
 
@@ -20,7 +22,10 @@ def populate_from_json(json):
             'username': t['user_name'],
             'text': t['tweet_text']
         }
-        tweets.insert_one(tweet)
+        try:
+            tweets.insert_one(tweet)
+        except OperationFailure as e:
+            print(e.details)
 
 
 def __main__():
