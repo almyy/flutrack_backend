fbfe6e9dc9f6c643483f97530f14bd84bda5d91e
Martin Almvik <martalm@stud.ntnu.no>
2016-02-23 15:21:44 +0100

    Getting error 400 on get. Trying to fix

--- flutrack_backend/settings.py
+++ flutrack_backend/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+secret_key = os.environ.get('SECRET_KEY')
+print(secret_key)
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
