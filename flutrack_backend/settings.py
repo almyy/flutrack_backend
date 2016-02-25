8ffcf9897d739f7d562b121f8df62fa3d04bce2c
Martin Almvik <martalm@ntnu.no>
2016-02-25 14:58:46 +0100

    Trying to fix 500 error

--- flutrack_backend/settings.py
+++ flutrack_backend/settings.py
@@ -24,6 +24,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
 
 ALLOWED_HOSTS = [".herokuapp.com"]
 
--- tweets/views.py
+++ tweets/views.py
@@ -12,14 +12,11 @@ import os
 def tweets(request):
     print("Received request. Fetching mongodb connection")
     mongo_uri = os.environ.get('MONGOLAB_URI')
-    if mongo_uri:
-        client = MongoClient(mongo_uri)
-        db = client.heroku_k99m6wnb
-        collection = db.tweets
-        cursor = collection.find()
-        print('Connected to DB')
-    else:
-        print("Mongodb_uri not available")
+    client = MongoClient(mongo_uri)
+    db = client.heroku_k99m6wnb
+    collection = db.tweets
+    cursor = collection.find()
+    print('Connected to DB')
 
     if request.method == 'GET':
         returned_tweets = []
