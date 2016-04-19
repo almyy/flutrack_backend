886ab0c00bd2a4df808c68acd9d9c717ac1d5c55
Mikael Rino Solstad <mikaelrino@hotmail.com>
2016-04-19 13:56:01 +0200

    fix

--- prediction/distribute_city_population.py
+++ prediction/distribute_city_population.py
@@ -5,7 +5,6 @@ import csv
 import os
 
 from pymongo import MongoClient
-from pandas import *
 
 from airport import airport
 from prediction.distribution_initiation import init_distributions
@@ -77,11 +76,13 @@ def get_removed_h(t):
 
 # Returns the probability that a latent individual becomes infectious on day t (5) gamma(t)
 def latent_becomes_infectious(t):
+    result = (get_latent_f(t) - get_latent_f(t + 1)) / get_latent_f(t)
     return float(infection_distribution[3][t])
 
 
 # Returns the probability that an infectious individual recovers on day t   (6) delta(t)
 def infectious_recovers(t):
+    result = (get_removed_h(t + 1) - get_removed_h(t)) / get_infectious_g(t)
     return float(infection_distribution[4][t])
 
 
@@ -153,14 +154,11 @@ class City:
         self.inf_res = {}
         self.location = location
 
-
     def get_latent_iterative(self, tau, t):
         return self.lat_res[tau, t]
 
-
     def get_sus_iterative(self, t):
         return self.sus_res[t]
-
 
     def apply_omega_latent(self, func, tau, t):
         help_sum = 0
@@ -175,7 +173,6 @@ class City:
         if result * tmp < 1:
             result = 0
         return result
-
 
     def apply_omega_sus(self, func, t):
         help_sum = 0
@@ -196,7 +193,6 @@ class City:
             result += latent_becomes_infectious(tau) * self.lat_res[tau, t - 1]
         return fraction_of_newly_ill_reported * result
 
-
     # Adds the four states in the city class to ensure that they are equal to the population count.
     def calculate_city_population(self):
         return int(self.susceptible + self.latent + self.infectious + self.recovered)
@@ -214,35 +210,33 @@ def initiate_validation_results():
     first_travel_day = 0
     initiate_influenza()
     initiate_initial_conditions(first_travel_day)
-    return city_list[City.index_city_id]
+    # return city_list[City.index_city_id]
+
+initiate_validation_results()
+hong_kong = city_list[9]
+new_york = city_list[0]
+days = 250
+result_matrix = []
+tmp_res = []
+
+for city in city_list:
+    tmp_res.append(city.name)
+result_matrix.append(tmp_res)
+
+tmp_res = []
+for t in range(0, days):
+    calculate_state_equations(t)
+    # print(hong_kong)
+
+    for city in city_list:
+        # tmp_res.append(int(city.get_daily_computed_morbidity(t + 1)))
+        tmp_res.append(int(city.lat_res[0, t]))
+    result_matrix.append(tmp_res)
+    tmp_res = []
 
 
-# initiate_validation_results()
-# hong_kong = city_list[9]
-# new_york = city_list[0]
-# days = 250
-# result_matrix = []
-# tmp_res = []
-#
-# for city in city_list:
-#     tmp_res.append(city.name)
-# result_matrix.append(tmp_res)
-#
-# tmp_res = []
-# for t in range(0, days):
-#     calculate_state_equations(t)
-#     # print(hong_kong)
-#
-#     for city in city_list:
-#         tmp_res.append(int(city.get_daily_computed_morbidity(t + 1)))
-#         # tmp_res.append(int(city.lat_res[0, t]))
-#     result_matrix.append(tmp_res)
-#     tmp_res = []
-#
-#
-# s = [[str(e) for e in row] for row in result_matrix]
-# lens = [max(map(len, col)) for col in zip(*s)]
-# fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
-# table = [fmt.format(*row) for row in s]
-# print('\n'.join(table))
-# # print(DataFrame(result_matrix))
+s = [[str(e) for e in row] for row in result_matrix]
+lens = [max(map(len, col)) for col in zip(*s)]
+fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
+table = [fmt.format(*row) for row in s]
+print('\n'.join(table))
