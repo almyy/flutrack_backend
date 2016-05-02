6e4bf601079df1bb08efdd9296a784b0a2b9cf54
Martin Almvik <martinalmvik@hotmail.com>
2016-05-02 14:43:38 +0200

    Removed API endpoint parameters

--- prediction/views.py
+++ prediction/views.py
@@ -6,4 +6,4 @@ from prediction import distribute_city_population
 @api_view(['GET'])
 def prediction(request):
     if request.method == 'GET':
-        return Response(distribute_city_population.forecast(int(request.query_params.get('index')), int(request.query_params.get('day'))))
+        return Response(distribute_city_population.forecast())
