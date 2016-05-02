80737277256d0b2d930bcd3147e6522e682b913c
Mikael Rino Strand Solstad <mikaelrs@ntnu.no>
2016-05-02 15:53:55 +0200

    Fix

--- prediction/distribute_city_population.py
+++ prediction/distribute_city_population.py
@@ -220,17 +220,14 @@ def initiate_validation_results(index_city):
 
 
 def forecast():
-    index_city = 14
+    index_city = 9
     initiate_validation_results(index_city)
     forecast_obj = []
     for t in range(0, forecast_horizon):
         calculate_state_equations(t)
         data = []
         for city in city_list:
-            data.append({'city': city.name, 'morbidity': city.daily_morbidity[t], 'location': city.location})
+            morbidity = int(city.daily_morbidity[t] / (city.population / 100000))
+            data.append({'city': city.name, 'morbidity': morbidity, 'location': city.location})
         forecast_obj.append(data)
     return forecast_obj
-
-obj = forecast()
-for list in obj:
-    print(list)
