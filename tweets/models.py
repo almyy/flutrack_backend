c42767cd8cdfe61f93a62d8f868fc314e777527a
Martin Almvik <martinalmvik@hotmail.com>
2016-05-03 15:10:04 +0200

    Fixing prod

--- tweets/models.py
+++ tweets/models.py
@@ -2,7 +2,6 @@ class Tweet(object):
     def __init__(self, lat, lng, username, text):
         self.lat = lat
         self.lng = lng
-        self.username = username
         self.text = text
 
 
--- tweets/serializers.py
+++ tweets/serializers.py
@@ -11,15 +11,13 @@ class TweetSerializer(serializers.Serializer):
 
     lat = serializers.CharField(required=False, max_length=50)
     lng = serializers.CharField(required=False, max_length=50)
-    username = serializers.CharField(required=True, max_length=50)
     text = serializers.CharField(required=False, max_length=100)
 
     def restore_object(self, attrs, instance=None):
         if instance:
             instance.lat = attrs.get('lat', instance.lat)
             instance.lng = attrs.get('lng', instance.lng)
-            instance.username = attrs.get('username', instance.username)
             instance.text = attrs.get('text', instance.text)
             return instance
 
-        return Tweet(attrs.get('lat'), attrs.get('lng'), attrs.get('username'), attrs.get('text'))
+        return Tweet(attrs.get('lat'), attrs.get('lng'), attrs.get('text'))
