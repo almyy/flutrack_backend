1a79154dcac25d643c1f8c0edbc63e6b8230f370
Martin Almvik <martinalmvik@hotmail.com>
2016-05-13 13:16:07 +0200

    Added mongolab uri to os.environ in setup

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ import os
 from flutrack_backend import populateDB
 
 os.environ['SECRET_KEY'] = 'm%nec7j8_r1yr3bkc=-(&qf91m17bfk35v5ct$ub-7n9=oscwd'
+os.environ['MONGOLAB_URI'] = 'mongodb://localhost:'
 populateDB.populate_from_txt("prediction/data/dummypopulation.csv")
 populateDB.populate_from_flutrack_api()
