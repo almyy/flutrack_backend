53891fdf22694d5acbfbe2071c1832e58d977b8e
Martin Almvik <martinalmvik@hotmail.com>
2016-06-06 15:26:22 +0200

    Updated ReadMe

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
-
+import sys
 os.environ['SECRET_KEY'] = 'm%nec7j8_r1yr3bkc=-(&qf91m17bfk35v5ct$ub-7n9=oscwd'
 os.environ['MONGOLAB_URI'] = 'mongodb://localhost:27017'
+os.environ['PYTHONPATH'] = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print("Added ")
